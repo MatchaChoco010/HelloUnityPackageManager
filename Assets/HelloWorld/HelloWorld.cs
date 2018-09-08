cb3f4e04f19087c31db87fbad75510168370ef35
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2018-09-08 22:29:19 +0900

    create some upm packages

---
+++ Assets/HelloWorld/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace net.matcha_choco010.HelloWorld {
+	public class HelloWorld : MonoBehaviour {
+
+		void Start () {
+			Debug.Log ("Hello World!");
+		}
+
+	}
+
+}
---
+++ Assets/SingletonMonoBehavior/SingletonMonoBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace net.matcha_choco010.SingletonMonoBehaviour {
+
+	public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T> {
+
+		private static T instance;
+		public static T Instance {
+			get {
+				if (instance == null) {
+					instance = (T) FindObjectOfType (typeof (T));
+
+					if (instance == null) {
+						Debug.LogError ($"{typeof(T)} is nothing");
+					}
+				}
+				return instance;
+			}
+		}
+
+		protected void Awake () {
+			if (this != Instance) {
+				Debug.LogError ($"{typeof(T)} is duplicated");
+			}
+		}
+
+	}
+
+}
